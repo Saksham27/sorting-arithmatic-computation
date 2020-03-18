c0dcc3e887e2d2cea834b2a3bdc7d73988806815
Saksham27 <geekysaksham27@gmail.com>
2020-03-18 18:58:04 +0530

    Added: operation for calculating a+b*c

--- sortingArithmaticComputation.sh
+++ sortingArithmaticComputation.sh
@@ -1,6 +1,9 @@
 #!/bin/bash -x
 
 # taking input three numbers
-read -p "Enter first number : " num1
-read -p "Enter second number : " num2
-read -p "Enter third number : " num3
+read -p "Enter first number a : " num1
+read -p "Enter second number b : " num2
+read -p "Enter third number c : " num3
+
+# compute a+b*c
+operation1=`echo "scale=2; $((num1+num2*num3))" | bc`
