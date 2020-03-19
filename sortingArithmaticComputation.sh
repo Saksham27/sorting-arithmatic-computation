5444c94f5ee66a1b6de7d7b754eff5e6fcc903cf
Saksham27 <geekysaksham27@gmail.com>
2020-03-19 11:33:47 +0530

    Added: added dictionary to store the result of every operation

--- sortingArithmaticComputation.sh
+++ sortingArithmaticComputation.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -x
 
+# initialising a dictionary
+declare -A operationResult
+
 # taking input three numbers
 read -p "Enter first number a : " num1
 read -p "Enter second number b : " num2
@@ -7,12 +10,18 @@ read -p "Enter third number c : " num3
 
 # compute a+b*c
 operation1=`echo "scale=2; $((num1+num2*num3))" | bc`
+operationResult["a+b*c"]=$operation1 # storing in dictionary
 
 # compute a*b+c
 operation2=`echo "scale=2; $((num1*num2+num3))" | bc`
+operationResult["a*b+c"]=$operation2 # storing in dictionary
 
 # compute c+a/b
 operation3=`echo "scale=2; ($num3+$num1/$num2)" | bc`
+operationResult["c+a/b"]=$operation3 # storing in dictionary
 
 # compute a%b+c
 operation4=`echo "scale=2; $((num1%num2+num3))" | bc`
+operationResult["a%b+c"]=$operation4 # storing in dictionary
+
+echo ${operationResult[@]}
